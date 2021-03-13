8c0b6349e127c65ce861769b2d4fd7315428d8bc
AasthaSinha2305 <55781193+AasthaSinha2305@users.noreply.github.com>
2021-03-13 16:42:43 +0530

    navbar & header done for feedpage

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import './App.css';
 import store from './store/store';
 import SignUp from './Component/SignUp/SignUp';
+import { FeedPage } from './Component/feedpage/FeedPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <div>
           <Switch>
             <Route path="/signup" component={SignUp} />
+            <Route path="/feedpage" component={FeedPage} />
           </Switch>
         </div>
       </div>
