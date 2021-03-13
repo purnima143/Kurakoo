eff4129d7c4d6ec942fe4fa4f3ea7efb0a4b921a
Himanshu Jaidka <himanshu.jaidka@gmail.com>
2021-03-13 20:57:56 +0530

    updated

---
+++ server/src/models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        min: 3,
+        max: 20
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        min: 3,
+        max: 20
+    },
+    userName: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        index: true,
+        lowercase: true
+
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true
+    },
+    hash_password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['user'],
+        default: 'user'
+    },
+    contactNumber: { type: String },
+    profilePicture: { type: String}
+
+})
---
+++ server/src/routes/auth.js
@@ -0,0 +1,3 @@
+const express = require("express");
+const { signup, signin } = require("./");
+const { isValidatesignin, isValidate}
