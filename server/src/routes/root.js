8aea24394f4a21243cf23ece65159f80f85c621d
Himanshu Jaidka <himanshu.jaidka@gmail.com>
2021-03-14 18:05:55 +0530

    Update root.js

--- server/src/routes/root.js
+++ server/src/routes/root.js
@@ -5,6 +5,6 @@ const example = require('./example.route');
 const authRoutes = require("./routes/auth");
 
 router.use('/example', example);
-router.use("/api", authRoutes);
+router.use("/", authRoutes);
 
 module.exports = router;
