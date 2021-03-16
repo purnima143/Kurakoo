f610cb55af20fc0946a448905022f909cf7d8685
Aniket1103 <55594740+Aniket1103@users.noreply.github.com>
2021-03-16 19:57:24 +0530

    decomposed prop parameter

--- client/src/components/feedComponent.js
+++ client/src/components/feedComponent.js
@@ -15,7 +15,7 @@ const useStyles = makeStyles({
   },
 });
 
-function Feed(props) {
+function Feed({ username, question, img, profession, answer }) {
   const [likeDislike, setLikeDislike] = useState([false, false]);
 
     const classes = useStyles();
@@ -23,19 +23,19 @@ function Feed(props) {
       <div>
         <section className = "feed-component">
           <div className = "bio-div">
-            <img src = { props.img } id = "profile-pic" alt = "img" />
+            <img src = { img } id = "profile-pic" alt = "img" />
             <div id = "bio-details">
-              <h4 id = "username">{ props.username }</h4>
-              <small>{ props.profession }</small>
+              <h4 id = "username">{ username }</h4>
+              <small>{ profession }</small>
             </div>
           </div>
           <hr/>
           <div id = "feed-content">
             <h4 id = "question">
-              { props.question }
+              { question }
             </h4>
             <p id = "answer">
-              { props.answer }
+              { answer }
             </p>
           </div>
           <hr/>
