cc7d3c52165272245974a4ade4e99a7f6cdd3a77
Aniket1103 <55594740+Aniket1103@users.noreply.github.com>
2021-03-23 01:10:01 +0530

    basic structure added - notification page

--- client/src/Component/NotificationPage/NotificationPage.js
+++ client/src/Component/NotificationPage/NotificationPage.js
@@ -8,9 +8,39 @@ function NotificationPage(){
     return (
         <div>
             <Navbar></Navbar>
-            
+            <br/><br/><br/><br/><br/><br/>
+            <div id = "notification-container">
+                <div className ="greet-msg">
+                    <div className ="img-div">
+
+                    </div>
+                    <div className = "content-div">
+                        <div className = "content-head">
+                            <h1>👋 Good Evening, Aniket </h1>
+                        </div>
+                        <br></br>
+                        <div>
+                            <p>
+                                You have 3 new messages, 1 new follower and you are accepted as a member at The Heritage
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
 
 export default NotificationPage;
+
+const data = {
+    currentuser: {
+        name: "Purnima",
+        msgCount: 3,
+        followersCount: 1
+    },
+
+    user1:{
+        username: "Emma Watson",
+    }
+}
