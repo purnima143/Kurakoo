0e885a82e318df5ab2d49f9114ce15e3574d8dba
Sristi27 <sristi2705@gmail.com>
2021-03-26 19:45:37 +0530

    added navbar in feed page

--- client/src/Component/navbar/Navbar.js
+++ client/src/Component/navbar/Navbar.js
@@ -7,35 +7,64 @@ export class Navbar extends Component {
 
   render() {
     return (
-        <div className="Nav">
-            <div className="nav1">
-                <div id="navbar" className="row">
-                    <div className="col col1">
-                        <div className="logo_nav">
-                            <a href="#home">
-                                <img className="logo_kurakoo"  src="./images/kurakoo-logo.png" alt="kurakoo_logo"/>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="col col2">
-                        <div className="seachbar_nav">
-                            <SearchBar />
-                        </div>
-                    </div>
+         // <div className="Nav">
+        //     <div className="nav1">
+        //         <div id="navbar" className="row">
+        //             <div className="col col1">
+        //                 <div className="logo_nav">
+        //                     <a href="#home">
+        //                         <img className="logo_kurakoo"
+        //                           src="./images/kurakoo-logo.png" alt="kurakoo_logo"/>
+        //                     </a>
+        //                 </div>
+        //             </div>
+        //             <div className="col col2">
+        //                 <div className="seachbar_nav">
+        //                     <SearchBar />
+        //                 </div>
+        //             </div>
 
-                    <div class="col col4">
-                        <div class="sign_nav">
-                            <div className="col profile_div">
-                                <img className="profile_img"  src="./images/profile_pic.jpg" alt="progile_image"/>
-                            </div>
-                            <div className="col btn_div">
-                                <button className="sign_in_btn"><b>Sign In</b></button>
-                            </div>
-                        </div>
+        //             <div class="col col4">
+        //                 <div class="sign_nav">
+        //                     <div className="col profile_div">
+        //                         <img className="profile_img"  src="./images/profile_pic.jpg" alt="progile_image"/>
+        //                     </div>
+        //                     <div className="col btn_div">
+        //                         <button className="sign_in_btn"><b>Sign In</b></button>
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+
+
+        // <div className="Nav">
+
+
+
+        //modified the navbar to be displayed in the Feed Page
+        //code added by Sristi Chowdhury
+        <nav className="navheader">
+        {/* <div className="logo_nav"> */}
+                        <a href="#home">
+                            <img className="logo_kurakoo"
+                              src="./images/kurakoo-logo.png" alt="kurakoo_logo"/>
+                        </a>
+                    {/* </div> */}
+                    <div className="searchbar_nav">
+                        <SearchBar/>
                     </div>
-                </div>
-            </div>
-        </div>
+                    <div class="sign_nav"> 
+                        {/* <div className="profile_div">  */}
+                            <img className="profile_img"  src="./images/profile_pic.jpg" alt="progile_image"/>
+                        {/* </div> */}
+                        {/* <div className="btn_div"> */}
+                            <button className="sign_in_btn"><b>Sign Out</b></button>
+                        {/* </div> */}
+                    </div>
+        </nav>
+    // </div>
     );
   }
 }
