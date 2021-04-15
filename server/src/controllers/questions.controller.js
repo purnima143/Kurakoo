90de562346eef336149163be85ee3be3619fbf19
Himanshu Jaidka <himanshu.jaidka@gmail.com>
2021-04-15 11:10:55 +0530

    added

--- server/src/controllers/questions.controller.js
+++ server/src/controllers/questions.controller.js
@@ -8,18 +8,18 @@ const createQuestions = (req, res) => {
     } = req.body;
 
 
-const question = new Questions({
-    questionText: questionText,
-    tags: tags,
-    createdBy: req.user._id
-});
+    const question = new Questions({
+        questionText: questionText,
+        tags: tags,
+        createdBy: req.user._id
+    });
 
-question.save(((error, question) => {
-    if(error) return res.status(400).json(responseHandler(false, 400, "Something went wrong!"));
-    if(question){
-        res.status(200).json(responseHandler(true, 200, question));
-    }
-}));
+    question.save(((error, question) => {
+        if(error) return res.status(400).json(responseHandler(false, 400, "Something went wrong!"));
+        if(question){
+            res.status(200).json(responseHandler(true, 200, question));
+        }
+    }));
 }
 
 module.exports = questionController = {
--- server/src/routes/root.js
+++ server/src/routes/root.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const example = require("./example.route");
 const authRoutes = require("./auth.routes");
 const questionRoutes = require("./questions.routes");
+const answerRoutes = require("./answers.routes");
 
 router.use("/example", example);
 router.use("/", authRoutes);
 router.use("/", questionRoutes);
+router.use("/", answerRoutes);
 
 module.exports = router;
