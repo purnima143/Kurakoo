18619e052392a9ee3c11ba27ff270b8fb359cdff
mitalirs <mitalirs1701@gmail.com>
2021-04-21 20:34:03 +0530

    added validation in the createAnswer route
    added 2 checks:
    1. no 2 answers by the same user for a question
    2. the user enters a valid questionId while posting the answer

--- server/src/controllers/answers.controller.js
+++ server/src/controllers/answers.controller.js
@@ -1,31 +1,55 @@
 const Answers = require("../models/answers.model");
 const responseHandler = require("../helpers/responseHandler");
+const Question = require("../models/questions.models")
+const ObjectID = require('mongodb').ObjectID;
 
-const createAnswers = (req, res) => {
+const createAnswers = async (req, res) => {
     const { answerText, questionId, tags } = req.body;
     
-     const answer = new Answers({
-        answerText: answerText,
-        tags: tags,
-        questionId: questionId,
-        createdBy: req.user._id
-    });
-
-    answer.save((error, answer) => {
-        if (error)
-            return res
-                .status(400)
-                .json(
-                    responseHandler(
-                        false,
-                        400,
-                        "Please fill the required fields"
-                    )
-                );
-        if (answer) {
-            res.status(200).json(responseHandler(true, 200, answer));
+    if(ObjectID.isValid(questionId)){  //checking if the string entered is a valid ObjectId
+        try{
+            const isValidQuestion = await Question.findById(questionId)  //if valid ObjectId, checking if a question with this id exists
+            if(isValidQuestion){
+                const duplicate = await Answers.findOne({createdBy: req.user._id, questionId: questionId}) //after 2 checks checking if there is already an answer created by the user for the same question
+                if(!duplicate){
+                    const answer = new Answers({
+                        answerText: answerText,
+                        tags: tags,
+                        questionId: questionId,
+                        createdBy: req.user._id
+                    });
+                    answer.save((error, answer) => {
+                        if (error)
+                            return res
+                                .status(400)
+                                .json(
+                                    responseHandler(
+                                        false,
+                                        400,
+                                        "Please fill the required fields"
+                                    )
+                                );
+                        if (answer) {
+                            return res.status(200).json(responseHandler(true, 200, answer));
+                        }
+                    });
+                }
+                else{
+                    return res.status(400).json(responseHandler(true, 400, {message: "you have already answered that question! wish to edit that?"}));
+                }
+            }
+            else{
+                return res.status(400).json(responseHandler(true, 400, {message: "invalid questioId!"}));
+            }
         }
-    });
+        catch(e){
+            return res.status(400).json(responseHandler(true, 400, {message: "something went wrong!"}));
+        }    
+    }
+    else{
+        return res.status(400).json(responseHandler(true, 400, {message: "invalid questioId!"}));
+    }     
+    
 };
 
 const deleteAnswer = async(req, res) => {
