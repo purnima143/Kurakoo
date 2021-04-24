ac7c13e80ccd2629e4f6a22b7e20db7524f5275c
Himanshu Jaidka <himanshu.jaidka@gmail.com>
2021-04-24 15:48:33 +0530

    connected signin

--- client/src/components/SignIn/SignIn.js
+++ client/src/components/SignIn/SignIn.js
@@ -5,77 +5,37 @@ import Grid from "@material-ui/core/Grid";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Link, useHistory } from "react-router-dom";
+import { login } from "../../actions/auth.constants";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
 toast.configure();
 
-const initialState = {
-    email: "",
-    password: ""
-};
 const SignIn = () => {
     const history = useHistory();
-
-    //STATE HOOK FOR INPUT DETAILS
-    const [formData, setFormData] = useState(initialState);
-
-    //MAKING CHANGE IN STATE VALUES FROM USER INPUT
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
-
     //CHECK WHETHER THE SCREEN IS SMALL OR NOT
     const isSmallScreen = useMediaQuery({
         query: "(max-width: 959.5px)"
     });
 
-    //TOAST TO DISPLAY FOR INVALID INPUTS WITH CUSTOM MESSAGE PARAMETER
-    const errorToast = (message) => {
-        toast.error(message, {
-            position: "top-center",
-            autoClose: 3000,
-            closeOnClick: true,
-            hideProgressBar: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined
-        });
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const auth = useSelector((state) => state.auth);
+
+    const dispatch = useDispatch();
+    const userLogin = (e) => {
+        e.preventDefault();
+
+        dispatch(
+            login({
+                email,
+                password
+            })
+        );
     };
 
-    //TOAST TO DISPLAY FOR SUCCESSFULL SIGNIN
-    const successToast = (message) => {
-        toast.success(message, {
-            position: "top-right",
-            autoClose: 2000,
-            hideProgressBar: true,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined
-        });
-    };
-
-    //FUNCTION TO DO APPROPRIATE TASK ON CLICKING SUBMIT BUTTON
-    const PostData = () => {
-        //CONDITIONS TO CHECK VALID INPUT DETAILS
-        if (formData.email !== "") {
-            // eslint-disable-next-line
-            const valid_email = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            if (valid_email.test(formData.email)) {
-                if (formData.password !== "") {
-                    //Code to perform authentication via an api
-                    // if user is successfully signed then then we can have a .then() block
-                    // in which we will show a toast and redirect the user
-                    successToast("Successfully signed in");
-                    history.push("/feed");
-                } else {
-                    errorToast("Please enter password");
-                }
-            } else {
-                errorToast("Please enter a valid email id");
-            }
-        } else {
-            errorToast("Please enter email");
-        }
-    };
+    if (auth.authenticate) {
+        return <Redirect to={"/"} />;
+    }
 
     return (
         <Grid container>
@@ -120,18 +80,20 @@ const SignIn = () => {
                                 type="text"
                                 placeholder="email"
                                 name="email"
-                                onChange={handleChange}
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                             />
                             <input
                                 type="password"
                                 placeholder="password"
                                 name="password"
-                                onChange={handleChange}
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
                             />
                         </div>
                     </form>
                     <footer>
-                        <button type="submit" onClick={PostData}>
+                        <button type="submit" onClick={userLogin}>
                             Continue
                         </button>
                         <p className="para">
