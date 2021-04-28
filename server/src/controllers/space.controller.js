fad89a1caa7e015c0ec694b220bf5d3f766c19c2
Himanshu Jaidka <himanshu.jaidka@gmail.com>
2021-04-28 23:20:49 +0530

    added space feature

---
+++ server/src/controllers/space.controller.js
@@ -0,0 +1,31 @@
+const Space = require("../models/spaces.models");
+const responseHandler = require("../helpers/responseHandler");
+
+const createSpaces = (req, res) => {
+    const { spaceName, description } = req.body;
+
+    const space = new Space({
+        spaceName: spaceName,
+        description: description,
+        createdBy: req.user._id
+    });
+
+
+    space.save(( error, space ) => {
+        if(error)
+            return res
+                .status(400)
+                .json(responseHandler(false, 400, "Space not created ! Something went wrong!"));
+        
+        if (space) {
+            res.json(responseHandler(true, 200, "Space Created successfully", space));
+        }
+    });
+};
+
+module.exports = spaceController = {
+    createSpaces
+};
+
+
+
---
+++ server/src/validators/space.validator.js
@@ -0,0 +1,25 @@
+const { check, validationResult } = require("express-validator");
+
+const validateSpaceRequest = [
+    check("spaceName")
+    .notEmpty()
+    .withMessage("spaceName is required"),
+    check("description")
+    .notEmpty()
+    .withMessage("description is Required")
+
+]
+
+const isRequestValidated = ( req, res, next ) => {
+    const errors =  validationResult(req);
+    if(errors.array().length > 0){
+        return res.status(400).json({ error: errors.array()[0].msg})
+    }
+    next();
+
+}
+
+module.exports = spaceValidator = {
+    validateSpaceRequest,
+    isRequestValidated
+};
