da2afd394184eb40f06f24952be9cfc6dc8e72c4
Subham <subhamy142@gmail.com>
2021-05-18 10:48:01 +0530

    added fetch by id endpoints

--- server/src/controllers/admin.controller.js
+++ server/src/controllers/admin.controller.js
@@ -2,6 +2,7 @@ const asyncHandler = require("express-async-handler")
 const User = require("../models/user.model");
 const Answer = require("../models/answers.model");
 const Question = require("../models/questions.models");
+const responseHandler = require("../helpers/responseHandler");
 
 
 // @desc    Get all users
@@ -31,8 +32,53 @@ const getQuestions = asyncHandler(async (req, res) => {
   }) 
 
 
+// @desc    Get  answer by Id
+// @route   GET /admin/user/:id
+// @access  Private/Admin
+const getAnswerById = asyncHandler(async (req, res) => {
+  const answer = await Answer.findById(req.params.id)
+
+  if(answer){
+    res.json(answer)
+  }else{
+    res.status(404)
+      .json(responseHandler(false, 404, "Answer not found", null));
+  }
+})
+
+// @desc    Get  question by Id
+// @route   GET /admin/question/:id
+// @access  Private/Admin
+const getQuestionById = asyncHandler(async (req, res) => {
+  const question = await Question.findById(req.params.id)
+
+  if(question){
+    res.json(question)
+  }else{
+    res.status(404)
+      .json(responseHandler(false, 404, "question not found", null));
+  }
+})
+
+
+// @desc    Get  user by Id
+// @route   GET /admin/user/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if(user){
+    res.json(user)
+  }else{
+    res.status(404)
+      .json(responseHandler(false, 404, "User not found", null));
+  }
+})
 module.exports = adminController = {
     getAnswers,
     getQuestions,
-    getUsers
+    getUsers,
+    getQuestionById,
+    getAnswerById,
+    getUserById
 };
