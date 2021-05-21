666b6be109ab6c4b42d4c5b7e82e2e8567440ff1
mitalirs <mitalirs1701@gmail.com>
2021-05-21 13:32:19 +0530

    added delete and update comment routes
    1. user can delete and update a comment using comment id
    2. a user has to be authenticated
    3. the comment id is passed through query string and has to be a valid Object id
    4. the comment should have been created by the user for him/her to update/delete it

--- server/src/routes/comments.routes.js
+++ server/src/routes/comments.routes.js
@@ -9,5 +9,7 @@ const commentController = require('../controllers/comments.controller');
 const router = express.Router();
 
 router.post('/postComment', requireSignin, userMiddleWare, commentController.postComment);
+router.patch('/updateComment/:id', requireSignin, userMiddleWare, commentController.updateComment)
+router.delete('/deleteComment/:id', requireSignin, userMiddleWare, commentController.deleteComment)
 
 module.exports = router;
