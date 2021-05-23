0266fea5afa0ba23a6cfb9b0f88c012c9de7ecac
Chitvan Ramani <chitvanramani@gmail.com>
2021-05-23 10:58:17 +0530

    aboutus

--- client/src/components/aboutus/aboutus.js
+++ client/src/components/aboutus/aboutus.js
@@ -1,51 +1,41 @@
 import React from "react";
 import "./aboutus.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import Navbar from '../navbar/Navbar'
+import Footer from '../Footer/footer'
 
 function aboutus() {
-    return <LandingPageorange {...landingPageorangeData} />;
+    return (<div><Navbar/><section class="text-center about">
+        
+    <h1>WHO ARE WE ?</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-sm-6 col-ex-12 about-item wow lightSpeedIn" data-wow-offset="200" >
+          <span class="fa fa-group"></span>
+          <h2>Grow Together</h2>
+          <p class="lead">A world of pleasure and pain. Something to share with our
+            dearest ones. Its a big growing family with the moto to shine and
+            improve together.</p>
+        </div>
+        <div class="col-lg-4 col-sm-6 col-ex-12 about-item wow lightSpeedIn" data-wow-offset="200">
+          <span class="fa fa-bullseye"></span>
+          <h2>Our Mission</h2>
+          <p class="lead"> Create a thriving community of engaged people, who share their time and knowledge</p></div>
+        <div class="clearfix visible-md-block visible-sm-block"></div>
+        <div class="col-lg-4 col-sm-6 col-ex-12 about-item wow lightSpeedIn" data-wow-offset="200">
+          <span class="fa fa-info"></span>
+          <h2>Everything in One</h2>
+          <p class="lead">Kurakoo in an online community of people providing answers to questions just like Quora site. Kurakoo is especially for scool college-students</p>
+        </div>
+        
+      </div>
+      
+    </div>
+  </section>
+  <Footer/>
+  </div>)
 }
 
 export default aboutus;
 
-function LandingPageorange(props) {
-    const { image3, text10, text2, text3, text4, text5, text6 } = props;
-    return (
-        <div className="grouping10">
-            <img className="image-3" src={image3} alt="img-3" />
-            <img
-                draggable={false}
-                className="mobile_logo_img"
-                src="../images/kurakoo-logo.png"
-                alt="mobile_logo_img"
-            ></img>
-            <h1 className="text-10 ">{text10}</h1>
-            <h2 className="text-2 montserrat-bold-black-24px">{text2}</h2>
-            <h3 class="title-text1">{text3}</h3>
-            <h4 class="title-text2">{text4}</h4>
-            <h4 class="title-text3">{text5}</h4>
-            <h4 class="text-6 ">{text6}</h4>
-        </div>
-    );
-}
-const landingPageorangeData = {
-    image3:
-        "https://anima-uploads.s3.amazonaws.com/projects/603e32473d832b6567888c87/releases/6053337a80ac5a0028404f78/img/image-3@1x.svg",
-    text10: <>WHO ARE WE?</>,
-    text2: (
-        <>
-            "KURAKOO" <br />
-            <br />A world of pleasure and pain. Something tO share with our
-            dearest ones. Its a big growing family with the moto to shine and
-            improve together.
-            <br /> <br />
-            -A place to share your knowledge and doubts.
-            <br />
-            <br />
-            -We guide you and let you guide others.
-        </>
-    ),
-    text3: <></>,
-    text4: <></>,
-    text5: <></>,
-    text6: <>"NOW YOUR LEARNING IS IN YOUR HANDS!" </>
-};
