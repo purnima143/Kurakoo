5e87751d3c940be2e80a7557a0df98d0cb15b888
Subham Yadav <60570595+Subham142@users.noreply.github.com>
2021-05-28 09:16:09 +0530

    admin user update action and reducer (#439)
    * admin userupdate action and reducer
    
    * added reducer to store

--- client/src/reducers/user.reducers.js
+++ client/src/reducers/user.reducers.js
@@ -2,7 +2,11 @@ import { userConstants,
         USER_LIST_FAIL,
         USER_LIST_REQUEST,
         USER_LIST_RESET,
-        USER_LIST_SUCCESS 
+        USER_LIST_SUCCESS, 
+        USER_UPDATE_FAIL, 
+        USER_UPDATE_REQUEST,
+        USER_UPDATE_SUCCESS,
+        USER_UPDATE_RESET
         } from "../actions/constants";
 
 const initState = {
@@ -51,5 +55,23 @@ export const userListReducer = (state = { users: [] }, action) => {
       default:
         return state
     }
-  }
+}
+ 
   
+export const userUpdateReducer = (state = { user: {} }, action) => {
+    switch (action.type) {
+      case USER_UPDATE_REQUEST:
+        return { loading: true }
+      case USER_UPDATE_SUCCESS:
+        return { loading: false, success: true }
+      case USER_UPDATE_FAIL:
+        return { loading: false, error: action.payload }
+      case USER_UPDATE_RESET:
+        return {
+          user: {},
+        }
+      default:
+        return state
+    }
+}
+
