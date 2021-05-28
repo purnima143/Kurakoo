e50b3e4d60401d3cc3b057b73a5e69f057068e3b
Subham <subhamy142@gmail.com>
2021-05-28 14:33:25 +0530

    user update action and reducer

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,11 +2,12 @@ import thunk from 'redux-thunk';
 import { applyMiddleware, createStore,combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-import {userListReducer, userUpdateReducer} from '../reducers/user.reducers'
+import {userListReducer, userUpdateReducer, userUpdateProfileReducer} from '../reducers/user.reducers'
 
 const reducer = combineReducers({
   userList: userListReducer,
   userUpdate: userUpdateReducer,
+  userUpdateProfile:userUpdateProfileReducer,
   })
 
 const middleware = [thunk]
