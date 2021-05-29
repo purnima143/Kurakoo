77bbbf12cc34e74ffe7a7794948c6da5f46f9422
Subham <subhamy142@gmail.com>
2021-05-30 00:58:13 +0530

    action and reducers to fetch answers

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,11 +3,13 @@ import { applyMiddleware, createStore,combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import {userListReducer, userUpdateReducer, userUpdateProfileReducer} from '../reducers/user.reducers'
+import { answerListReducer } from "../reducers/answer.reducer";
 
 const reducer = combineReducers({
   userList: userListReducer,
   userUpdate: userUpdateReducer,
-  userUpdateProfile:userUpdateProfileReducer,
+  userUpdateProfile: userUpdateProfileReducer,
+  answerList: answerListReducer,
   })
 
 const middleware = [thunk]
