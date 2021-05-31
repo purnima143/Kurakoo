d4e5eeaa0fc1e1235cda196b1f62361a844930bc
Himanshu <himanshu.jaidka@gmail.com>
2021-05-31 23:46:30 +0530

    Unit testing (#520)
    * install dependencies for the test
    
    * added the code for the signup
    
    * added code for the signin

--- server/src/test/signup.js
+++ server/src/test/signup.js
@@ -53,4 +53,54 @@ describe('POST /api/signup', () => {
           .expect(400)
           .end(done);
       });
+
 });
+
+describe('POST /api/signin', () => {
+    it('Should login user and return auth token', (done) => {
+      request(app)
+        .post('/api/signin')
+        .send({
+          email: users[1].email,
+          password: users[1].password
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.headers['x-auth']).not.toBeNull();
+        })
+        .end((err, res) => {
+          if(err) return done(err);
+  
+          User.findById(users[1]._id).then((user) => {
+            expect(user.tokens[1]).toHaveProperty('access', 'auth');
+            expect(user.tokens[1]).toHaveProperty('token', res.headers['x-auth']);
+            done();
+          }).catch((err) => done(err));
+        })
+    });
+  
+    it('Should reject invalid login', (done) => {
+        request(app)
+          .post('/api/signin')
+          .send({
+            email: users[1].email,
+            password: users[1].password +'1'
+          })
+          .expect(400)
+          .expect((res) => {
+            expect(res.headers['x-auth']).toBeUndefined();
+          })
+          .end((err, res) => {
+            if(err) return done(err);
+  
+            User.findById(users[1]._id).then((user) => {
+              expect(user.tokens.length).toBe(1)
+              done();
+            }).catch((err) => done(err));
+          })
+    });
+  });
+  
+
+});
+
