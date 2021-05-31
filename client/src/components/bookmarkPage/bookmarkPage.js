86df89e0f5e07a750b61f5f8acfaa134b6f2d313
Chitvan Ramani <chitvanramani@gmail.com>
2021-05-31 16:03:42 +0530

    bookmark page

--- client/src/components/bookmarkPage/bookmarkPage.js
+++ client/src/components/bookmarkPage/bookmarkPage.js
@@ -5,31 +5,13 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Meta from "../../helpers/Meta";
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import {Progress} from 'bootstrap-4-react';
 import {Grid,Typography} from '@material-ui/core';
-import '../SignUp/SignUp.css'
+import './bookmark.css'
 import Navbar from "../navbar/Navbar"
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-  heading:{
-      textAlign:"center"
-  },
-  formLabel:{
-      marginLeft:'20px',
-      fontSize:'20px',
-      fontWeight:600,
-      marginTop:'120px'
-
-
-  },
+ 
   formBox:{
      marginLeft:'20px', 
      width:"80%"
@@ -58,47 +40,42 @@ const SignUp = () => {
 
 
     
-
     if (auth.authenticate) {
-        return <Redirect to={"/"} />;
-    }
+      return <Redirect to={"/"} />;
+  }
 
-    if (user.loading) {
-        return <p>Loading...!</p>;
-    }
-
+  if (user.loading) {
+      return <Progress mb='4' w='25%'><Progress.Bar striped animated min='0'max='100' mx='auto' now='50'>Loading....</Progress.Bar></Progress>;
+  }
     return (
         <div>
-              <Navbar/>
-        <div className={"main"} style={{marginTop:"50px"}}>
+              <Navbar/><br/><br/>
+        <div className="main" style={{marginTop:"50px",backgroundColor:'white'}} >
           
             <Meta title="Bookmark Page ​🔖​| Kurakoo" />
             <Typography variant="h3" component="h2" align="center">
               <hr/>
-            <img alt="previe" src="https://freeiconshop.com/wp-content/uploads/edd/bookmark-alt-flat.png" width="80" className={classes.bookMark}/>  Bookmark Page&nbsp;
+            <img alt="previe" src="https://freeiconshop.com/wp-content/uploads/edd/bookmark-alt-flat.png" width="80" className={classes.bookMark} />  Bookmark Page&nbsp;
                 
 </Typography>
-            <Grid container spacing={1}>
-            <Grid item xs={6}>
-            <h2 className={classes.heading}>My Questions</h2>
-          <Paper>
-          <img alt="preview" src="https://previews.123rf.com/images/chrisdorney/chrisdorney1602/chrisdorney160200077/51658510-no-questions-asked-red-rubber-stamp-over-a-white-background-.jpg"
-    style={{width:"100%", height:"480px", opacity:0.5}}
-    />
-          </Paper>
-        </Grid>
-        <Grid item xs={6}>
-        <h2 className={classes.heading}>My Answers</h2>
-          <Paper>
-    <img alt="preview" src="https://previews.123rf.com/images/lkeskinen/lkeskinen1707/lkeskinen170708972/82453980-no-answer-rubber-stamp.jpg"
-    style={{width:"100%", height:"480px", opacity:0.5}}
-    />
-          </Paper>
-        </Grid>
-       
-            </Grid>
-        </div>
-        </div>
+<div/>
+<div class="card-container">
+  <div class="card">
+    <div class="side"><img alt="preview" src="https://previews.123rf.com/images/lkeskinen/lkeskinen1707/lkeskinen170708972/82453980-no-answer-rubber-stamp.jpg"
+    style={{width:"100%", height:"280px", opacity:0.5}}
+    /></div>
+    <div class="side back">No Answers</div>
+  </div>
+</div>
+<div class="card-container2">
+  <div class="card2">
+    <div class="side"><img alt="preview" src="https://previews.123rf.com/images/chrisdorney/chrisdorney1602/chrisdorney160200077/51658510-no-questions-asked-red-rubber-stamp-over-a-white-background-.jpg"
+    style={{width:"100%", height:"280px", opacity:0.5}}
+    /></div>
+    <div class="side back">No Questions</div>
+  </div>
+</div>
+        </div></div>
     );
 };
 
